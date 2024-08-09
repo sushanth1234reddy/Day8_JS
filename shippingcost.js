48a4a22e8d76213a3fbb58bb71ec19a0d60e6799
Singadwar Sushanth Reddy <76097402+sushanth1234reddy@users.noreply.github.com>
2024-08-09 23:37:42 +0530

    Add files via upload

---
+++ shippingcost.js
@@ -0,0 +1,44 @@
+// let region="Australia";
+// switch(region){
+//     case "North America":
+//         console.log("Cost is $10");
+//         break;
+//     case "South America":
+//         console.log("Cost is $20");
+//         break;
+//     case "Asia":
+//         console.log("Cost is $40");    
+//         break;
+//     case "Europe":
+//         console.log("Cost is $30");
+//     case "Australia":
+//         console.log("Cost is $50");
+//         break;
+//     default:
+//         console.log("Enter correct region");    
+// }
+
+// program using functions
+function vinod(region){
+    switch(region){
+    case "North America":
+        console.log("Cost is $10");
+        break;
+    case "South America":
+        console.log("Cost is $20");
+        break;
+    case "Asia":
+        console.log("Cost is $40");    
+        break;
+    case "Europe":
+        console.log("Cost is $30");
+        break;
+    case "Australia":
+        console.log("Cost is $50");
+        break;
+    default:
+        console.log("Enter correct region");    
+}
+
+}
+vinod("Asia")
